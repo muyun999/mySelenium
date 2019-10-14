f09c7aa01bd8b412338a67cb2f21f9da5b1fa882
jinxuyun <752074370@qq.com>
2019-10-14 20:02:42 +0800

    原始文件

---
+++ mytest.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Firefox()
+url = "http://www.optima-trans.net/#/"
+driver.get(url)
+js1 = """document.querySelector('.k-datepicker-input').removeAttribute("readonly")"""
+driver.execute_script(js1)
+js2 = """document.querySelector(".k-datepicker-input").value='2018-11-11'"""
+driver.execute_script(js2)
+# elm.clear()
+# elm.send_keys("2017-10-10")
+# js3 = """document.querySelectorAll(".k-option")[1]"""
+# driver.execute_script(js3)
+js4 = """var elm = document.querySelectorAll(".k-option")[1];
+        elm.style.border="2px solid red";
+        elm.value='浙江省杭州市';
+"""
+driver.execute_script(js4)
+
+if __name__ == "__main__":
+    pass
+
